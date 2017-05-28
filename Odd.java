fbf22a1345a20f86bc32d8cb245924111cedb287
sambo talitha <tzwanbun@gmail.com>
2017-05-28 16:08:59 -0700

    Add files via upload

---
+++ Odd.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+public class Odd {
+
+	
+	
+	
+
+		public static void main(String args[]){
+			Scanner tuna = new Scanner (System.in);
+			
+				
+			int		
+ boy;
+			
+			System.out.println("Enter a value");
+			boy=tuna.nextInt();
+	if( boy % 2 == 0){
+				
+				System.out.println("Number is even");
+			}
+			
+			
+			else{
+				System.out.println("Number is odd");
+			
+			}
+
+			
+			
+			
+			
+		}
+	}
+
+
